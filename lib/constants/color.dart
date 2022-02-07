24a9968b895fb4fc51fcbe13ffa4e5d567a1f06c
derciosinione <derciosinione@gmail.com>
2022-02-07 01:27:22 +0100

    Creating Nav Bar

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -4,6 +4,7 @@ class AppColors {
   static const Color light = Color(0xFFF7F8FC);
   static const Color lightBackground = Color(0xFFF9F9F9);
   static const Color lightGrey1 = Color(0xFFA4A6B3);
+  static const Color lightGrey2 = Color(0xFFC5C7CD);
   static const Color dark = Color(0xFF212121);
   static const Color dark1 = Color(0xFF363740);
   static const Color activeLight = Color(0xFF512DA8);
--- lib/large_screen.dart
+++ lib/large_screen.dart
@@ -16,18 +16,77 @@ class LargeScreen extends StatelessWidget {
           flex: 5,
           child: Column(
             children: [
-              Container(
-                height: 65,
-                decoration: const BoxDecoration(
-                color: Colors.white,
-                boxShadow: [BoxShadow()]
-                ),
-              ),
+              const NavBar(),
               Expanded(
                 child: Container(color: Colors.transparent),
               )
             ],
           )),
     ]);
+  }
+}
+
+class NavBar extends StatelessWidget {
+  const NavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 0,
+          blurRadius: 1,
+          offset: const Offset(0, .2),
+        )
+      ]),
+      child: Row(children: [
+        const SizedBox(width: 20),
+        InkWell(
+          onTap: () {
+            print("Large Menu licked");
+          },
+          child: const Icon(Icons.menu_outlined, size: 30),
+        ),
+        const SizedBox(width: 20),
+        const CustomText(
+          text: "Dashboard",
+          color: AppColors.dark,
+          size: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        const Expanded(child: SizedBox()),
+        InkWell(
+          child: Icon(Icons.settings, color: AppColors.dark1.withOpacity(.7)),
+          onTap: () {},
+        ),
+        const SizedBox(width: 20),
+        Stack(
+          children: [
+            InkWell(
+                child: Icon(Icons.notifications,
+                    color: AppColors.dark1.withOpacity(.7)),
+                onTap: () {}),
+            Positioned(
+              top: 2,
+              right: 2,
+              child: Container(
+                width: 12,
+                height: 12,
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: AppColors.light, width: 2),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
   }
 }
