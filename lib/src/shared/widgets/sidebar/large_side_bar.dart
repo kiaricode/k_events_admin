9f0460a410c00b3d1656a9bad04c4da05250a6c0
derciosinione <derciosinione@gmail.com>
2022-02-09 11:47:59 +0100

    Created Side bar Header

--- lib/src/shared/widgets/sidebar/large_side_bar.dart
+++ lib/src/shared/widgets/sidebar/large_side_bar.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:k_events_admin/src/shared/widgets/custom_text.dart';
 
 import '../../themes/app_colors.dart';
 import 'side_bar_header.dart';
