1bc166c47a98745ce5aa8ef1efea615eb2f668b9
derciosinione <derciosinione@gmail.com>
2022-02-09 11:42:10 +0100

    Created Side bar Header

--- lib/src/shared/widgets/large_side_bar.dart
+++ lib/src/shared/widgets/large_side_bar.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:k_events_admin/src/shared/widgets/custom_text.dart';
 
 import '../themes/app_colors.dart';
-
-
 
 class LargeSideBar extends StatelessWidget {
   const LargeSideBar({
@@ -12,8 +12,31 @@ class LargeSideBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 250,
+      width: 260,
       color: AppColors.dark,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: Center(
+              child: Text.rich(
+                TextSpan(
+                    text: "KIARI",
+                    style: GoogleFonts.asap(
+                        color: AppColors.activeLight,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                    children: const [
+                      TextSpan(
+                          text: " EVENTS",
+                          style: TextStyle(color: Colors.white))
+                    ]),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
